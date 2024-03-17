6e635d8c00d110d7a1609b1ca3043b95f00d0eab
Cali Garmo <aram.dermenjian.o@gmail.com>
2024-03-17 14:06:40 +0100

    Models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    booking_date = models.DateTimeField()
+    no_of_guests = models.SmallIntegerField()
+
+    def __str__(self): 
+        return self.name
+
+
+# Add code to create Menu model
+class Menu(models.Model):
+   title = models.CharField(max_length=255) 
+   price = models.DecimalField( max_digits=10, decimal_places=2)
+   inventory = models.IntegerField()
+
+   def __str__(self):
+      return self.title
